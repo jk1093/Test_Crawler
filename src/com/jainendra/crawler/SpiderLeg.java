693617891bea4a14254bb7d818c3c81fe732b3e6
j_kumar <jainendrakumar10@yahoo.com>
2018-01-22 00:47:25 +0530

    Update SpiderLeg.java

--- src/com/jainendra/crawler/SpiderLeg.java
+++ src/com/jainendra/crawler/SpiderLeg.java
@@ -10,15 +10,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class SpiderLeg
 {
-    private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
     private List<String> links = new LinkedList<String>();
     private Document htmlDocument;
+    private static final String agent ="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    
     public boolean crawl(String url)
     {
         try
         {
-            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Connection connection = Jsoup.connect(url).userAgent(agent);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
             if(connection.response().statusCode() == 200) 
